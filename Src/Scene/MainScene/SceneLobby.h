c859cb7bed7b8dfa66219fd52708a24ace14f5c2
eguma\riku <raito11092005@outlook.jp>
2026-05-20 08:53:16 +0900

    シーンロビーを追加

--- Src/Scene/MainScene/SceneLobby.h
+++ Src/Scene/MainScene/SceneLobby.h
@@ -1,5 +1,22 @@
 #pragma once
-class SceneLobby
+
+#include "../SceneBase.h"
+
+class SceneLobby : public SceneBase
 {
+public:
+
+	SceneLobby(void);
+
+	~SceneLobby(void);
+
+	void Load(void) override;
+
+	void Initialize(void) override;
+
+	void Update(void) override;
+
+
+private:
 };
 
