af828c330a14392c4bda26cd8e58c61da71fe3eb
airen1dog@softbank.ne.jp <airen1dog@softbank.ne.jp>
2026-05-29 15:20:42 +0900

    追加

---
+++ Src/Object/Actor/Chara/Weapon/Bullet/Boss/BBulletMG.cpp
@@ -0,0 +1,49 @@
+#include "BBulletMG.h"
+
+BBulletMG::BBulletMG(Transform& _transform)
+{
+}
+
+BBulletMG::~BBulletMG(void)
+{
+}
+
+void BBulletMG::Load(void)
+{
+}
+
+void BBulletMG::ReleasePost(void)
+{
+}
+
+void BBulletMG::SetPos(VECTOR _pos)
+{
+}
+
+void BBulletMG::InitTransform(void)
+{
+}
+
+void BBulletMG::InitCollider(void)
+{
+}
+
+void BBulletMG::InitAnimation(void)
+{
+}
+
+void BBulletMG::InitPost(void)
+{
+}
+
+void BBulletMG::UpdateProcess(void)
+{
+}
+
+void BBulletMG::UpdateProcessPost(void)
+{
+}
+
+void BBulletMG::DrawPre(void)
+{
+}
---
+++ Src/Object/Actor/Chara/Weapon/Bullet/Boss/BBulletMG.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "BBulletBase.h"
+class BBulletMG :
+    public BBulletBase
+{
+public:
+	BBulletMG(Transform& _transform);
+	~BBulletMG(void)override;
+
+	// リソースロード
+	void Load(void) override;
+
+	void ReleasePost(void)override;
+
+
+
+	void SetIsAttac(bool _isAttac)override { isAttac_ = _isAttac; }
+
+	void SetPos(VECTOR _pos);
+protected:
+	// 大きさ、回転、座標の初期化
+	void InitTransform(void) override;
+
+	// 衝突判定の初期化
+	void InitCollider(void) override;
+
+	// アニメーションの初期化
+	void InitAnimation(void) override;
+
+	// 初期化後の個別処理
+	void InitPost(void) override;
+
+	void UpdateProcess(void) override;
+	void UpdateProcessPost(void) override;
+
+
+
+	// 前描画
+	void DrawPre(void) override;
+
+
+
+
+
+private:
+};
+
