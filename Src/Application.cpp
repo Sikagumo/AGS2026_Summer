ca7fcbd17ea27874842d4586513e81fe76e117bf
eguma\riku <raito11092005@outlook.jp>
2026-07-16 12:10:51 +0900

    複数ウィンド開けるように調整

--- Src/Application.cpp
+++ Src/Application.cpp
@@ -63,6 +63,7 @@ void Application::Init(void)
 	SetUseDirect3DVersion(DX_DIRECT3D_11);
 	isInitFail_ = false;
 	SetMultiThreadFlag(true);
+	SetDoubleStartValidFlag(true);
 
 	if (DxLib_Init() == -1)
 	{
