54892746b03c2868ebbbf204685dbcab73bd8e02
Sikagumo111 <Sikagumo@users.noreply.github.com>
2026-07-23 16:36:29 +0900

    プレイヤースペルミス修正

--- Src/Manager/Generic/SceneManager.cpp
+++ Src/Manager/Generic/SceneManager.cpp
@@ -204,7 +204,7 @@ void SceneManager::Update(void)
         }
 
         //ChangeScene(std::make_shared<SceneTitle>());
-        auto jobs = { SceneGame::PlayerSelectType(PlayerBase::JOB_TYPE::BOMB, PlayerBase::SKIN_TYPE::DOG)};
+        auto jobs = { SceneGame::PlayerSelectType(PlayerBase::JOB_TYPE::BOMB, PlayerBase::SKIN_TYPE::HUMAN)};
         ChangeScene(std::make_shared<SceneGame>(jobs));
 
         return;
